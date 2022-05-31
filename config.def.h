97bfc043e434cf2b4b15f72cefc2dcc4e5350ed0
Himanshu <himanshugoswami.12feb@gmail.com>
2022-06-01 00:24:49 +0530

    Firefox defaults to third tag. Added retrowave theme

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx          = 3;        /* border pixel of windows */
-static const unsigned int gappx             = 15;        /* gaps between windows */
+static const unsigned int gappx             = 16;        /* gaps between windows */
 static const unsigned int snap              = 32;       /* snap pixel */
 static const unsigned int systraypinning    = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft     = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -15,7 +15,7 @@ static const char *fonts[]          = {"RecMonoCasual Nerd Font:size=14", "Noto 
 static const char dmenufont[]       = "RecMonoCasual Nerd Font:size=14";
 //static const char dmenufont[]       = "monospace:size=10" ; 
 
-#include "themes/default.h" // For themes
+#include "themes/retrowave.h" // For themes
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
---
+++ themes/retrowave.h
@@ -0,0 +1,7 @@
+static const char norm_fg[] = "#9b80df";
+static const char norm_bg[] = "#0e030d";
+static const char norm_border[] = "#6c599c";
+
+static const char sel_fg[] = "#9b80df";
+static const char sel_bg[] = "#4D174D";
+static const char sel_border[] = "#9b80df";
